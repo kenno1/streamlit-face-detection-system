145df0e98d7bc83f385e73027db7c53d7160044b
kambe.kenta <kenta.k199405@gmail.com>
2021-02-11 16:46:21 +0900

    added data frame and magic command.

--- main.py
+++ main.py
@@ -5,3 +5,14 @@ import numpy as np
 st.title('Streamlit Face Detection app')
 
 st.write('DataFrame')
+st.write(
+    pd.DataFrame({
+        '1st column': [1, 2, 3, 4],
+        '2nd column': [10, 20, 30, 40]
+    })
+)
+
+"""
+# this is explain.
+Magic Command 
+"""
